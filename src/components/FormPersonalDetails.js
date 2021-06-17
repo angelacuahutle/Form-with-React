e4d4533a2544b26281a61a354ae324cddf3633fa
angelacuahutle <angela.cuahutle@gmail.com>
2021-06-17 08:41:09 -0500

    Update FormPersonalDetails

--- src/components/FormPersonalDetails.js
+++ src/components/FormPersonalDetails.js
@@ -1,63 +1,76 @@
 import React, { Component } from 'react';
-import FormUserDetails from './FormUserDetails';
- 
-export class UserPersonalForm extends Component {
-    state = {
-      step: 1,
-      firstName: '',
-      lasName: '',
-      email: '',
-      occupations: '',
-      city: '',
-      lastName: '',
-      bio: ''
-    };
+import Dialog from '@material-ui/core/Dialog';
+import AppBar from '@material-ui/core/AppBar';
+import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
-    // Proceed to next step
-    nextStep = () => {
-        const { step } = this.state;
-        this.setState({
-            step: step + 1
-        });
-    };
+export class FormPersonalDetails extends Component {
+  continue = e => {
+    e.preventDefault();
+    this.props.nextStep();
+  };
 
-    // Go back to prev step
-    prevStep = () => {
-        const { step } = this.state;
-        this.setState({
-            step: step - 1
-        });
-    };
+  back = e => {
+    e.preventDefault();
+    this.props.prevStep();
+  };
 
-    //Handle fields change 
-    handleChange = input => e => {
-        this.setState({[input]: e.target.value});
-    };    
-        
-    render() {
-        const { step } = this.state;
-        const{ firstName, lastName, email, occupation, city,
-        bio } = this.state;
-        const values = { firstName, lastName, email, 
-        occupation, city, bio };
-        // eslint-disable-next-line
-        switch(step) {
-           case 1:
-             return (
-                 <FormUserDetails
-                   nextStep={this.nextStep}
-                   handleChange={this.handleChange}
-                   values={values}
-                 />
-             );
-        case 2:
-            return <hi>FormPersonalDetails</hi>
-        case 3:
-            return <hi>Confirm</hi>  
-        case 4:
-            return <hi>Success</hi>         
-        } 
-    }
+  render() {
+    const { values, handleChange } = this.props;
+    return (
+      <MuiThemeProvider>
+        <>
+          <Dialog
+            open
+            fullWidth
+            maxWidth='sm'
+          >
+            <AppBar title="Enter Personal Details" />
+            <TextField
+              placeholder="Enter Your Occupation"
+              label="Occupation"
+              onChange={handleChange('occupation')}
+              defaultValue={values.occupation}
+              margin="normal"
+              fullWidth
+            />
+            <br />
+            <TextField
+              placeholder="Enter Your City"
+              label="City"
+              onChange={handleChange('city')}
+              defaultValue={values.city}
+              margin="normal"
+              fullWidth
+            />
+            <br />
+            <TextField
+              placeholder="Enter Your Bio"
+              label="Bio"
+              onChange={handleChange('bio')}
+              defaultValue={values.bio}
+              margin="normal"
+              fullWidth
+            />
+            <br />
+
+            <Button
+              color="secondary"
+              variant="contained"
+              onClick={this.back}
+            >Back</Button>
+
+            <Button
+              color="primary"
+              variant="contained"
+              onClick={this.continue}
+            >Continue</Button>
+          </Dialog>
+        </>
+      </MuiThemeProvider>
+    );
+  }
 }
 
-export default FormPersonalDetails; 
+export default FormPersonalDetails;
