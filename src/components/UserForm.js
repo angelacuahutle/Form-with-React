98166b938c80141d48e160135e808dcba192ebdf
AngelMoraDev <angelmomaa@gmail.com>
2021-06-17 08:30:48 -0500

    fix errors in user form

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -38,7 +38,7 @@ export class UserForm extends Component {
     const { step } = this.state;
     const { firstName, lastName, email, occupation, city, bio } = this.state;
     const values = {
-      firstName, lastName, email, occupation, city, bio };
+      firstName, lastName, email, occupation, city, bio  };
     // eslint-disable-next-line
     switch (step) {
       case 1:
