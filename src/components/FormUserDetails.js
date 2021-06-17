ddef19bfff45adeb36d948bf5f81d1ff806754c7
angelacuahutle <angela.cuahutle@gmail.com>
2021-06-17 08:43:15 -0500

    Update FormUserDetails

--- src/components/FormUserDetails.js
+++ src/components/FormUserDetails.js
@@ -1,63 +1,64 @@
 import React, { Component } from 'react';
-import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+import Dialog from '@material-ui/core/Dialog';
 import AppBar from '@material-ui/core/AppBar';
+import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 
-export class FormPersonalDetails extends Component {
+export class FormUserDetails extends Component {
   continue = e => {
     e.preventDefault();
-    this.props.nextStep()
+    this.props.nextStep();
   };
 
   render() {
     const { values, handleChange } = this.props;
     return (
       <MuiThemeProvider>
-        <React.Fragment>
-          <AppBar title="Enter User Details" />
-          <TextField
-            hintText="Enter Your First Name"
-            floatingLabelText="Frist Name"
-            onChange={handleChange('firstName')}
-            defaultValue={values.firstName}
-          />
-          <br />
-          <TextField
-            hintText="Enter Your Last Name"
-            floatingLebelText="Last Name"
-            onChange={handleChange('lastName')}
-            defaultValue={values.lastName}
-          />
-          <br />
-          <TextField
-            hintText="Enter Your Email"
-            floatingLebelText="Email"
-            onChange={handleChange('email')}
-            defaultValue={values.email}
-          />
-          <br />
-          <Button
-            variant="contained"
-            label="Continue"
-            primary={true}
-            style={styles.button}
-            onClick={this.contiunue}
-          />
-        </React.Fragment>
+        <>
+          <Dialog
+            open
+            fullWidth
+            maxWidth='sm'
+          >
+            <AppBar title="Enter User Details" />
+            <TextField
+              placeholder="Enter Your First Name"
+              label="First Name"
+              onChange={handleChange('firstName')}
+              defaultValue={values.firstName}
+              margin="normal"
+              fullWidth
+            />
+            <br />
+            <TextField
+              placeholder="Enter Your Last Name"
+              label="Last Name"
+              onChange={handleChange('lastName')}
+              defaultValue={values.lastName}
+              margin="normal"
+              fullWidth
+            />
+            <br />
+            <TextField
+              placeholder="Enter Your Email"
+              label="Email"
+              onChange={handleChange('email')}
+              defaultValue={values.email}
+              margin="normal"
+              fullWidth
+            />
+            <br />
+            <Button
+              color="primary"
+              variant="contained"
+              onClick={this.continue}
+            >Continue</Button>
+          </Dialog>
+        </>
       </MuiThemeProvider>
     );
   }
 }
 
-const styles = {
-  button: {
-    margin: 15,
-  }
-}
-
-<<<<<<< HEAD
-export default FormPersonalDetails;
-=======
 export default FormUserDetails;
->>>>>>> 98166b938c80141d48e160135e808dcba192ebdf
