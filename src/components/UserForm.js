8ac219e5b38f7a7d09ef1999773086f461c0754f
angelacuahutle <angela.cuahutle@gmail.com>
2021-06-17 08:46:16 -0500

    Update FormUserDetails

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -1,11 +1,7 @@
 import React, { Component } from 'react';
 import FormUserDetails from './FormUserDetails';
-<<<<<<< HEAD
 import FormPersonalDetails from './FormPersonalDetails';
  
-=======
-
->>>>>>> 98166b938c80141d48e160135e808dcba192ebdf
 export class UserForm extends Component {
   state = {
     step: 1,
@@ -39,7 +35,6 @@ export class UserForm extends Component {
     this.setState({ [input]: e.target.value });
   };
 
-<<<<<<< HEAD
     //Handle fields change 
     handleChange = input => e => {
         this.setState({[input]: e.target.value});
@@ -74,32 +69,8 @@ export class UserForm extends Component {
         case 4:
             return <hi>Success</hi>         
         } 
-=======
-  render() {
-    const { step } = this.state;
-    const { firstName, lastName, email, occupation, city, bio } = this.state;
-    const values = {
-      firstName, lastName, email, occupation, city, bio  };
-    // eslint-disable-next-line
-    switch (step) {
-      case 1:
-        return (
-          <FormUserDetails
-            nextStep={this.nextStep}
-            handleChange={this.handleChange}
-            values={values}
-          />
-        );
-      case 2:
-        return <hi>FormPersonalDetails</hi>
-      case 3:
-        return <hi>Confirm</hi>
-      case 4:
-        return <hi>Success</hi>
->>>>>>> 98166b938c80141d48e160135e808dcba192ebdf
     }
   }
-}
 
 export default UserForm;
 
