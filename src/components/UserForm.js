ce90a512dfbf314ba495ed00cd51ea40b24d44c3
angelacuahutle <angela.cuahutle@gmail.com>
2021-06-17 08:48:22 -0500

    Update UserForm

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
 import FormUserDetails from './FormUserDetails';
 import FormPersonalDetails from './FormPersonalDetails';
- 
+import Confirm from './Confirm';
+import Success from './Success';
+
 export class UserForm extends Component {
   state = {
     step: 1,
     firstName: '',
-    lasName: '',
-    email: '',
-    occupations: '',
-    city: '',
     lastName: '',
+    email: '',
+    occupation: '',
+    city: '',
     bio: ''
   };
 
@@ -30,47 +31,49 @@ export class UserForm extends Component {
     });
   };
 
-  //Handle fields change 
+  // Handle fields change
   handleChange = input => e => {
     this.setState({ [input]: e.target.value });
   };
 
-    //Handle fields change 
-    handleChange = input => e => {
-        this.setState({[input]: e.target.value});
-    };    
-        
-    render() {
-        const { step } = this.state;
-        const{ firstName, lastName, email, occupation, city,
-        bio } = this.state;
-        const values = { firstName, lastName, email, 
-        occupation, city, bio };
-        // eslint-disable-next-line
-        switch(step) {
-           case 1:
-             return (
-                 <FormUserDetails
-                   nextStep={this.nextStep}
-                   handleChange={this.handleChange}
-                   values={values}
-                 />
-             );
-        case 2:
-            return  (
-                <FormPersonalDetails
-                  nextStep={this.nextStep}
-                  handleChange={this.handleChange}
-                  values={values}
-                />
-            );    
-        case 3:
-            return <hi>Confirm</hi>  
-        case 4:
-            return <hi>Success</hi>         
-        } 
+  render() {
+    const { step } = this.state;
+    const { firstName, lastName, email, occupation, city, bio } = this.state;
+    const values = { firstName, lastName, email, occupation, city, bio };
+
+    switch (step) {
+      case 1:
+        return (
+          <FormUserDetails
+            nextStep={this.nextStep}
+            handleChange={this.handleChange}
+            values={values}
+          />
+        );
+      case 2:
+        return (
+          <FormPersonalDetails
+            nextStep={this.nextStep}
+            prevStep={this.prevStep}
+            handleChange={this.handleChange}
+            values={values}
+          />
+        );
+      case 3:
+        return (
+          <Confirm
+            nextStep={this.nextStep}
+            prevStep={this.prevStep}
+            values={values}
+          />
+        );
+      case 4:
+        return <Success />;
+      default:
+        (console.log('This is a multi-step form built with React.'))
     }
   }
+}
 
 export default UserForm;
 
